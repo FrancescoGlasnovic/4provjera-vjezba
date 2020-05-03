6d4557aabe58d758c8de7beb6c98ab4f50aa04a9
FrancescoGlasnovic <64422825+FrancescoGlasnovic@users.noreply.github.com>
2020-05-03 22:57:17 +0200

    Zadatak 5.
    Ispisuju svi podaci uzlazno sortirani po prezimenu i imenu

--- main.cpp
+++ main.cpp
@@ -45,6 +45,10 @@ int main()
     unsigned long long int *brRacuna=new unsigned long long int[1000];
     string *prezimeIme=new string[1000];
     double *saldo=new double[1000];
+    unsigned long long int *brRacuna2=new unsigned long long int[1000];
+    string *prezimeIme2=new string[1000];
+    double *saldo2=new double[1000];
+
 
         while(1)
         {
@@ -133,6 +137,33 @@ int main()
                         }
                         if(i==brKlijenata)
                             cout<<"Trazenog broja nema.";
+
+                    }
+
+            else if(izbor==5)
+                    {
+                        cout<<"Racuni sortirani A-Z"<<endl;
+                        copy(prezimeIme,prezimeIme+brKlijenata,prezimeIme2);
+                        copy(saldo,saldo+brKlijenata,saldo2);
+                        copy(brRacuna,brRacuna+brKlijenata,brRacuna2);
+                        for(int i=0;i<brKlijenata;i++)
+                        {
+                            for(int j=brKlijenata-1;j>i;j--)
+                            {
+                              if(prezimeIme[i]>prezimeIme[i+1]){
+                                  swap(prezimeIme2[i],prezimeIme2[i+1]);
+                                  swap(saldo2[i],saldo2[i+1]);
+                                  swap(brRacuna2[i],brRacuna2[i+1]);
+                              }
+                            }
+
+                        }
+
+                            for(int i=0; i<brKlijenata; i++)
+                            {
+                                cout << brRacuna2[i]<< ", "<< prezimeIme2[i]<<", "<<saldo2[i]<<endl;
+                            }
+
 
                     }
 
